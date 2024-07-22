2b07736fc9d70dd8c00506cc4cf095d805f537e0
c-mccormick007 <c.mccormick1295@gmail.com>
2024-07-22 17:52:48 -0500

    fix dependencies

--- src/js/services/user-created-rounds.service.ts
+++ src/js/services/user-created-rounds.service.ts
@@ -71,6 +71,7 @@ class UserCreatedRounds {
         let updatedRounds = Object.assign({}, savedRounds, {
             [round.pdgaNumber]: updatedPlayerRounds
         });
+
         return this.setStoredRounds(updatedRounds).pipe(
             mapTo(round)
         );
@@ -100,6 +101,7 @@ class UserCreatedRounds {
 
     clearPlayer(pdgaNumber: string) {
         let savedRounds = Object.assign({}, this.savedRoundsSubject.getValue());
+        
         delete savedRounds[pdgaNumber] 
         return this.setStoredRounds(savedRounds).pipe(
             mapTo(savedRounds)
